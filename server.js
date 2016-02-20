4b5e8e04be8b5d9a36380591c6cbcd149c012a82
darthyoshi007 <doshi.parth9@gmail.com>
2016-02-20 15:44:48 -0800

    server + client

---
+++ server.js
@@ -0,0 +1,8 @@
+(function() {
+var express = require('express');
+var app = express();
+
+server.listen(8080, function() {
+    return console.log('Server listening at port 8080');
+});
+}).call(this);
