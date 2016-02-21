62e5cefdf06d6e03eaf1c13f93c084f87f206e7a
KingManatee <mitcheldowney16@mittymonarch.com>
2016-02-20 17:19:20 -0800

    facebook integration

--- server.js
+++ server.js
@@ -5,6 +5,22 @@ server.listen(8080, function() {
     return console.log('Server listening at port 8080');
 });
 
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '960585080655610',
+      xfbml      : true,
+      version    : 'v2.5'
+    });
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+
 server.get('/', function (req, res) {
   res.send('Hello World!');
 });
