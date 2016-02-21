3731581f43a1717d2e4c42f94820c504097f9e0f
darthyoshi007 <doshi.parth9@gmail.com>
2016-02-20 16:14:01 -0800

    does something
    maybe

--- server.js
+++ server.js
@@ -6,4 +6,7 @@ var server = require('http').createServer(app);
 server.listen(8080, function() {
     return console.log('Server listening at port 8080');
 });
+
+app.use(express["static"]('../'));
+
 }).call(this);
